687e413b23773516af7dc42dd7d1ae649f3e0554
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2019-04-22 23:27:22 -0300

    Alguns ajustes nos argumentos e nomes.

--- codes/main.py
+++ codes/main.py
@@ -4,16 +4,16 @@ import pre_processing
 from VGG19 import VGG19
 
 parser = argparse.ArgumentParser(description='Script para executar tarefas gerais como: fazer o pré-processamento e em seguida treinar, treinar a rede diretamente.')
-parser.add_argument('--spect_folders', action='store', dest='spect_folders', default='../dados/spect', required='training' in sys.argv, help='Nome/caminho da pasta contendo as pastas com os espectrogramas.')
+parser.add_argument('--spect', action='store', dest='spect_folders', default='../dados/spect', required='training' in sys.argv, help='Nome/caminho da pasta contendo as pastas com os espectrogramas.')
 parser.add_argument('--mode', choices=['training', 'all_proc'], action='store', dest='mode', default='../dados/spect', required=True, help='Modo em que o arquivo deve ser executado. training: somente o treinamente. all_proc: pre-processar e treinar.')
 parser.add_argument('--csv', action='store', dest='csv', default='../dados/db-spect.csv', required='all_proc' in sys.argv, help='Nome/caminho do .csv com os dados para categorizar.')
-parser.add_argument('--audio_folders', action='store', dest='audio_folders', default='../dados/pac-audios', required='all_proc' in sys.argv, help='Nome/caminho da pasta contendo as pastas com os áudios.')
+parser.add_argument('--audio', action='store', dest='audio_folders', default='../dados/pac-audios', required='all_proc' in sys.argv, help='Nome/caminho da pasta contendo as pastas com os áudios.')
 arguments = parser.parse_args()
 
 if(arguments.mode == 'training'):
     print("Entering on training mode...")
     vgg19 = VGG19()
-    vgg19.config_db(arguments.spect_folders)    
+    vgg19.config_dataset(arguments.spect_folders)    
     vgg19.train()
 else:
     print('Entering on all mode...')    
@@ -22,5 +22,5 @@ else:
 
     print('Starting training...')
     vgg19 = VGG19()
-    vgg19.config_db(path_spect)    
+    vgg19.config_dataset(path_spect)    
     vgg19.train()
