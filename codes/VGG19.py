0a2247e3ae4dcd033acf264ad1e134a7b747cf29
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2019-04-22 23:24:39 -0300

    Implementação do predict próprio para o problema e o seu teste.

--- codes/VGG19.py
+++ codes/VGG19.py
@@ -12,6 +12,7 @@ from tensorflow.keras.models import Sequential, model_from_json, load_model
 
 import pre_processing
 import matplotlib.pyplot as plt
+import multiprocessing
 
 class VGG19:
 
@@ -96,15 +97,16 @@ class VGG19:
             'labels': []
         }
                 
-        dirname, _ = os.path.split(os.path.abspath(__file__))   
+                
         if(not os.path.isabs(path_to_spect_folders)):
+            dirname, _ = os.path.split(os.path.abspath(__file__))   
             path_to_spect_folders = os.path.join(dirname, path_to_spect_folders)
         
-        folders = os.listdir(path_to_spect_folders) # lista de pastas (classes)
-        indexes = list(range(len(folders))) #criando uma lista com os índeces
+        self.folders = os.listdir(path_to_spect_folders) # lista de pastas (classes)
+        indexes = list(range(len(self.folders))) #criando uma lista com os índeces
         one_hot_encondings = to_categorical(np.array(indexes)) # one_hot_enconding para todos as pastas (classes)
 
-        for folder in folders:
+        for folder in self.folders:
             
             print('folder: {}'.format(folder))
 
@@ -115,14 +117,15 @@ class VGG19:
                 
                 im = cv2.imread(os.path.join(path_to_spect_folders, folder, files[i]))
                 if(i < int(len(files)*0.9)):                    
-                    self.training_set['pics'].append(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
-                    self.training_set['labels'].append(one_hot_encondings[folders.index(folder)])
+                    self.training_set['pics'].append(cv2.resize(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), (224, 224)))
+                    self.training_set['labels'].append(one_hot_encondings[self.folders.index(folder)])                    
                 else:                    
-                    self.test_set['pics'].append(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
-                    self.test_set['labels'].append(one_hot_encondings[folders.index(folder)])               
-        
+                    self.test_set['pics'].append(cv2.resize(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), (224, 224)))
+                    self.test_set['labels'].append(one_hot_encondings[self.folders.index(folder)])               
+                    
         print('training and validation set: {} images'.format(len(self.training_set['pics'])))
         print('test set: {} images'.format(len(self.test_set['pics'])))
+        
 
     def train(self):
         '''
@@ -142,10 +145,18 @@ class VGG19:
 
         sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)        
         # loss='sparse_categorical_crossentropy'
-        callbacks_list = [keras.callbacks.ModelCheckpoint('./best_weights.h5', monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=True)]
+        callbacks_list = [keras.callbacks.ModelCheckpoint('./best_weights.h5', monitor='val_acc', verbose=1, 
+                                                          save_best_only=True, save_weights_only=True)]
+        
+        
         self.model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
-        history = self.model.fit(x=np.array(self.training_set['pics']).reshape(-1, 224, 224, 3), y=np.array(self.training_set['labels']), batch_size=16, epochs=15, validation_split=0.111, callbacks=callbacks_list)        
-        evaluate = self.model.evaluate(np.array(self.test_set['pics']).reshape(-1, 224, 224, 3), np.array(self.test_set['labels']))         
+        
+        history = self.model.fit(x=np.array(self.training_set['pics']).reshape(-1, 224, 224, 3), 
+                                 y=np.array(self.training_set['labels']), batch_size=16, epochs=20, 
+                                 validation_split=0.111, callbacks=callbacks_list)        
+        
+        evaluate = self.model.evaluate(np.array(self.test_set['pics']).reshape(-1, 224, 224, 3), 
+                                       np.array(self.test_set['labels']))         
         
         print(evaluate)
 
@@ -191,39 +202,66 @@ class VGG19:
             path_to_audio = os.path.join(dirname, path_to_audio)
 
         audio_list = pre_processing.split_audio(path_to_audio, 200)
-        print('Audios splitted in rate of {} ms by section.'.format(200))
+        print('------> Audios splitted in rate of {} ms by section.'.format(200))
         
         spectrograms_folder_temp = os.path.join(path_to_audio, '../temp')
         os.makedirs(spectrograms_folder_temp, exist_ok=True)
         for i in range(len(audio_list)):            
             pre_processing.wav2spectrogram(audio_list[i], os.path.join(spectrograms_folder_temp, '{}.png'.format(i)))
-        print('Images saved in a temp folder. Images total: {}'.format(len(audio_list)))
+        print('------> Images saved in a temp folder. Images total: {}'.format(len(audio_list)))
         
-        print('Predicting audio file from saved images...')
+        print('------> Predicting audio file from saved images...')
         pred = []
         for archive in os.listdir(spectrograms_folder_temp):
             im = cv2.imread(os.path.join(spectrograms_folder_temp, archive))
-            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-            pred.append(self.model.predict(np.array(im).reshape(-1, 224, 224, 3)).flatten())
+            im = cv2.resize(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), (224, 224))
+            temp = self.model.predict(np.array(im).reshape(-1, 224, 224, 3)).flatten()
+            pred.append(temp)
+        
+        
+        if pred == []:
+            return -1
         
         result_pred = [0]*len(pred[0])
         for i in range(len(pred)):
             for j in range(len(pred[i])):                
                 result_pred[j] += pred[i][j]
         
-        print('Non-standardized result', result_pred)
+        print('------> Non-standardized result', result_pred)
         result_pred = result_pred/sum(result_pred)
-        print('Standardized result', result_pred)
+        print('------> Standardized result', result_pred)
+        category = np.argmax(result_pred)
+        print('------> Deviation type: {}'.format(self.folders[category]))
         
         os.system('rm -r {}'.format(spectrograms_folder_temp))
-        print('Deleted folder {}'.format(spectrograms_folder_temp))
+        print('------> Deleted folder {}'.format(spectrograms_folder_temp))
+        
+        
+        return self.folders[category]
+    
 
     def test_predict(self, path_to_class):
-        dirname, _ = os.path.split(os.path.abspath(__file__))  
+        
         if(not os.path.isabs(path_to_class)):
+            dirname, _ = os.path.split(os.path.abspath(__file__))  
             path_to_class = os.path.join(dirname, path_to_class)
 
+        right = 0
+        wrong = 0
+        err = 0
         for c in os.listdir(path_to_class):
             print('predict to {}'.format(c))
             for patient in os.listdir(os.path.join(path_to_class, c)):
-                self.predict(os.path.join(path_to_class, c, patient))
+                print('Patient {}'.format(patient))
+                category = self.predict(os.path.join(path_to_class, c, patient))
+                if(category == c):
+                    right += 1
+                elif(category == -1):
+                    err += 1
+                else:
+                    wrong += 1
+                print("right: {}; wrong: {}; error: {}".format(right, wrong, err))
+                print('-----------------------------------------------')
+        print('{}/{} right'.format(right, right+wrong+err))         
+        print('{}/{} wrong'.format(wrong, right+wrong+err))         
+        print('{}/{} with some erros in the files'.format(err, right+wrong+err))         
